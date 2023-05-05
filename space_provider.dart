bb9b8554f51a847be941a05c8cc9cf3003a1dd39
Muhammad Fadhil Hidayatullah <103361348+fadhil30@users.noreply.github.com>
2023-05-05 13:57:36 +0700

    Add files via upload

---
+++ space_provider.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class SpaceProvider extends ChangeNotifier {
+  getRecommendedSpaces() async {
+    var result = await http
+        .get(Uri.parse('https://bwa-cozy-api.vercel.app/recommended-spaces'));
+    print(result.statusCode);
+    print(result.body);
+  }
+}
